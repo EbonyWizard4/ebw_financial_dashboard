b832d71b9836a9fbb402a07df8e3bfe254df94d3
EbonyWizard4 <antonio.jhone@hotmail.com>
2024-06-27 08:44:12 -0300

    Captured form data and validated

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,1 +1,22 @@
-'user server'
+'use server'
+import { z } from "zod";
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoices = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoices(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoices.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+}
