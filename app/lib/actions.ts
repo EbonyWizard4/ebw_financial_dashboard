3152f78e51660ca83cb6593c65ebb11c9a848318
EbonyWizard4 <antonio.jhone@hotmail.com>
2024-06-28 22:38:20 -0300

    implemented error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,9 +13,9 @@ const FormSchema = z.object({
 
 const CreateInvoices = FormSchema.omit({ id: true, date: true });
 
-const UpdateInvoice = FormSchema.omit({id: true, date: true});
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id:string, formData:FormData) {
+export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -24,15 +24,18 @@ export async function updateInvoice(id:string, formData:FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
+    try {
+        await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
     `;
-
+    } catch (error) {
+        return { message: 'Database Error: Failed to Update  Invoices' };
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-    
+
 }
 
 export async function createInvoices(formData: FormData) {
@@ -41,19 +44,29 @@ export async function createInvoices(formData: FormData) {
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amount}, ${status}, ${date})
-    `;
-
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amount}, ${status}, ${date})
+        `;
+    } catch (error) {
+        return {
+            message: 'DAtabase Error: Failed to create Imvoice.',
+        };
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id:string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+export async function deleteInvoice(id: string) {
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
 }
