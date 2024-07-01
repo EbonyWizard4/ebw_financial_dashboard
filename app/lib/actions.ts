e3cea2c8a9d14cca932b802e670042f8f5dc9c2d
EbonyWizard4 <antonio.jhone@hotmail.com>
2024-07-01 20:34:17 -0300

    applied data validation by the server with ease of access message

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,11 +3,17 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import { error } from "console";
+
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please Select a Costumer!',
+    }),
+    amount: z.coerce.number().gt(0, {message: 'Please enter an amount greater than $0.'}),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please Select an invoice status.',
+    }),
     date: z.string(),
 });
 
@@ -37,14 +43,30 @@ export async function updateInvoice(id: string, formData: FormData) {
     redirect('/dashboard/invoices');
 
 }
+export type State  = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    }
+    message?: string | null;
+}
 
-export async function createInvoices(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoices.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoices.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -63,7 +85,7 @@ export async function createInvoices(formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    try {
+        try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
     } catch (error) {
